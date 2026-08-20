47d6dba2367dda3387cb8838881cd501dcac76cb
space-shell <j-n@hotmail.co.uk>
2026-08-20 17:52:46 +0200

    stage0: flake devshell with nodejs 22

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "unlimigent - spatial agent orchestration devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+          ];
+          shellHook = ''
+            echo "unlimigent devshell: node $(node --version)"
+          '';
+        };
+      });
+    };
+}
